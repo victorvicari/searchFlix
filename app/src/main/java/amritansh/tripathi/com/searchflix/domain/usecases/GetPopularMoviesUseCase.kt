126e094e6cdfd6ad9cf5ca6a23e4c06fa285562f
Amritansh Tripathi <amritanshtripathi@gmail.com>
2018-03-07 15:41:57 -0500

    Upadted the usecases

--- app/src/main/java/amritansh/tripathi/com/searchflix/domain/usecases/GetPopularMoviesUseCase.kt
+++ app/src/main/java/amritansh/tripathi/com/searchflix/domain/usecases/GetPopularMoviesUseCase.kt
@@ -1,11 +1,18 @@
-package amritansh.tripathi.com.searchflix.domain.usecases
+package amritansh.tripathi.com.searchflix.domain.interactors
 
+import amritansh.tripathi.com.searchflix.domain.usecases.GetPopularMoviesUseCase
+import amritansh.tripathi.com.searchflix.model.Repository
 import amritansh.tripathi.com.searchflix.network.Movie
+import amritansh.tripathi.com.searchflix.network.MovieResult
+import amritansh.tripathi.com.searchflix.utils.SchedulerProvider
 import io.reactivex.Observable
+import javax.inject.Inject
 
 /**
  * Created by amritanshtripathi on 3/5/18.
  */
-interface GetPopularMoviesUseCase {
-    fun getPopularMovieUseCase(): Observable<List<Movie>>
+class GetPopularMoviesInteractor @Inject constructor(private val repository: Repository, private val schedulerProvider: SchedulerProvider) : GetPopularMoviesUseCase {
+    override fun getPopularMovieUseCase(): Observable<List<Movie>> {
+        return repository.getPopularMovies().compose(schedulerProvider.getSchedulersForSingle()).map { it: MovieResult -> it.result }.toObservable()
+    }
 }
