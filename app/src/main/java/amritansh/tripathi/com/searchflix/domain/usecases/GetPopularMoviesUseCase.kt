973f2a6ad1effc46e3cc139fb90ecc1d03d2ad9a
Victor <victor.a.vicari@gmail.com>
2019-09-30 15:54:57 -0300

    rollback from Flowable to Observable... once there will be no backpressure.

--- app/src/main/java/amritansh/tripathi/com/searchflix/domain/usecases/GetPopularMoviesUseCase.kt
+++ app/src/main/java/amritansh/tripathi/com/searchflix/domain/usecases/GetPopularMoviesUseCase.kt
@@ -2,7 +2,7 @@ package amritansh.tripathi.com.searchflix.domain.usecases
 
 import amritansh.tripathi.com.searchflix.model.Repository
 import amritansh.tripathi.com.searchflix.network.MovieResult
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import javax.inject.Inject
 
 /**
@@ -10,7 +10,7 @@ import javax.inject.Inject
  */
 class GetPopularMoviesUseCase @Inject constructor(private val repository: Repository) {
 
-    fun getPopularMovieUseCase(page: Int, pageSize: Int): Flowable<MovieResult> =
+    fun getPopularMovieUseCase(page: Int, pageSize: Int): Observable<MovieResult> =
             repository.getPopularMovies(page, pageSize)
 
 }
--- app/src/main/java/amritansh/tripathi/com/searchflix/model/Repository.kt
+++ app/src/main/java/amritansh/tripathi/com/searchflix/model/Repository.kt
@@ -3,7 +3,7 @@ package amritansh.tripathi.com.searchflix.model
 import amritansh.tripathi.com.searchflix.network.ApiService
 import amritansh.tripathi.com.searchflix.network.MovieResult
 import amritansh.tripathi.com.searchflix.network.SimilarMoviesResult
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
@@ -12,7 +12,7 @@ import javax.inject.Inject
  */
 class Repository @Inject constructor(private val apiService: ApiService) {
 
-    fun getPopularMovies(page: Int, pageSize: Int): Flowable<MovieResult> = apiService.getPopularMovies(page, pageSize)
+    fun getPopularMovies(page: Int, pageSize: Int): Observable<MovieResult> = apiService.getPopularMovies(page, pageSize)
 
     fun search(search: String?): Single<MovieResult> = apiService.searchMovies(search)
 
